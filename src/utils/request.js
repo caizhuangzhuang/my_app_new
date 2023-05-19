c09a341ed605821919fa7d89a01f94d3459183d6
caizhuangzhuang <107240799+caizhuangzhuang@users.noreply.github.com>
2023-05-19 18:26:31 +0800

    [CHG]:合并分支

--- src/utils/request.js
+++ src/utils/request.js
@@ -73,3 +73,5 @@ instance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+export default instance;
--- src/views/Userinfo.vue
+++ src/views/Userinfo.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="user">
     <span>修改个人信息</span>
-    <img :src="dalImg(mydata.avatar)" alt="" class="avatar" />
+    <input type="file" style="display: none" ref="fileTag" />
+    <img
+      :src="dalImg(imgUrl)"
+      alt=""
+      class="avatar"
+      @click="selImg"
+      @change="selEnd"
+    />
     <p>年龄：{{ mydata.age }}</p>
     <p>昵称：{{ mydata.userName ? mydata.nickName : mydata.userName }}</p>
     <p>积分：{{ mydata.score }}</p>
@@ -27,16 +34,21 @@ import {
 import { dalImg } from "../utils/tools";
 import { loadUserserAPI } from "../services/user";
 import { changeuserinfoAPI } from "../services/userInfo";
+// 引入默认导出的网络请求instance
+import axios from "../utils/request";
 import "vant/lib/index.css";
 
 const router = useRouter();
 const mydata = ref([]);
+const imgUrl = ref("");
 const userAge = ref("");
+const fileTag = ref();
 const userNickName = ref("");
 
 async function loadUserser() {
   const result = await loadUserserAPI();
   mydata.value = result.data.data;
+  imgUrl.value = result.data.data.coverImage; //设置展示的头像的地址
   userAge.value = result.data.data.age;
   userNickName.value = result.data.data.nickName;
 }
@@ -45,6 +57,7 @@ const save = async () => {
     const result = await changeuserinfoAPI({
       nickName: userNickName.value,
       age: userAge.value,
+      avatar: imgUrl.value,
     });
     showSuccessToast(result.data.msg);
     // 修改成功后跳转到我的页面
@@ -52,6 +65,22 @@ const save = async () => {
   } else {
     showFailToast("请输入内容");
   }
+};
+
+// 点击图片出现选择图片
+const selImg = () => {
+  //触发这个标签的点击事件
+  fileTag.value.click();
+};
+// 选择图片后
+const selEnd = () => {
+  console.log(fileTag.value.files);
+  const formData = new FormData();
+  formData.append("file", fileTag.value.files[0]);
+  axios.post("/api/v1/common/upload_file", formData).then((res) => {
+    // 设置成功后设置上传成功的图片
+    imgUrl.value = res.data;
+  });
 };
 onMounted(() => {
   loadUserser();
